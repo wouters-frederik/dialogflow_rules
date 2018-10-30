7bff99fe298497c954e698456e1a455e9775eab0
Frederik Wouters <wouters_frederik@721548.no-reply.drupal.org>
2018-10-30 20:31:18 +0100

    added example module

---
+++ modules/dialogflow_example_intent/src/Plugin/Chatbot/Intent/DialogFlowExampleIntent.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\dialogflow_example_intent\Plugin\Chatbot\Intent;
+
+use Drupal\chatbot_api\Plugin\IntentPluginBase;
+use GuzzleHttp\Client;
+
+/**
+ * Plugin implementation of chatbot intent.
+ *
+ * @Intent(
+ *   id = "projects/restobot-hfb/agent/intents/22d0d4e1-b8b7-495e-a965-123cda6b2bd0",
+ *   label = @Translation("what is the weather?")
+ * )
+ */
+class DialogFlowExampleIntent extends IntentPluginBase {
+  private function gettext($temperature) {
+    $pre = $temperature. ' degrees celcius. ';
+    if ($temperature <= 0) {
+      return $pre . 'Freezing cold! ';
+    }elseif ($temperature > 0 && $temperature < 8) {
+       return $pre . 'Rather chilly! ';
+    }elseif ($temperature >= 8 && $temperature < 15) {
+     return $pre . "That's ok for this time of the year. ";
+    }elseif ($temperature >= 15 && $temperature < 55) {
+      return $pre . "That's quite warm. ";
+    }
+  }
+
+ /**
+   * Do the weather request
+   */
+  private function doWeatherRequest(){
+    $client = new Client([
+        'base_uri' => 'https://services.vrt.be/',
+      ]);
+      $response = $client->get('weather/observations/belgische_streken?accept=application%2Fvnd.weather.vrt.be.observations_1.0%2Bjson', [
+        'debug' => TRUE,
+        'headers' => [
+          'Content-Type' => 'application/x-www-form-urlencoded',
+        ]
+      ]);
+      $body = $response->getBody();
+      return json_decode((string) $body);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process() {
+    $data = $this->doWeatherRequest();
+    foreach( $data->observations as $observation) {
+      if ($observation->location == 'Centrum') {
+        $temperature = $observation->temperature;
+        $windspeed = $observation->wind->speed;
+        $this->response->setIntentResponse($this->gettext($temperature));
+        $this->response->setIntentDisplayCard($this->gettext($temperature), 'Greetings');
+      }
+    }
+  }
+}
---
+++ modules/dialogflow_example_intent/src/Plugin/Chatbot/Intent/DialogFlowJokeIntent.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\dialogflow_example_intent\Plugin\Chatbot\Intent;
+
+use Drupal\chatbot_api\Plugin\IntentPluginBase;
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Plugin implementation of chatbot intent.
+ *
+ * @Intent(
+ *   id = "projects/restobot-hfb/agent/intents/7327f61f-8305-456c-9e1b-f3938d287420",
+ *   label = @Translation("tell me a joke.")
+ * )
+ */
+class DialogFlowJokeIntent extends IntentPluginBase implements ContainerFactoryPluginInterface{
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+     * {@inheritdoc}
+     */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, RequestStack $request_stack) {
+      parent::__construct($configuration, $plugin_id, $plugin_definition);
+      $this->requestStack = $request_stack;
+    }
+
+
+  private function getName(){
+      $content = $this->requestStack->getCurrentRequest()->getContent();
+      $data = json_decode($content, true);
+      return $data['queryResult']['parameters']['name'];
+  }
+
+ /**
+   * Do the joke request
+   */
+  private function doJokeRequest($name){
+    if (!empty($name)) {
+      $url = 'jokes/random?firstName=' . $name ;
+    }else{
+      $url = 'jokes/random';
+    }
+    $client = new Client([
+      'base_uri' => 'http://api.icndb.com/',
+    ]);
+    $response = $client->get($url);
+    $body = $response->getBody();
+    return json_decode((string) $body);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process() {
+    $name = $this->getName();
+    $data = $this->doJokeRequest($name);
+    $this->response->setIntentResponse($data->value->joke);
+    $this->response->setIntentDisplayCard($data->value->joke, 'Greetings');
+  }
+}
