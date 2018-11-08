37fb53543b010d003fc0b3739deccf58d797b052
Frederik Wouters <wouters_frederik@721548.no-reply.drupal.org>
2018-11-08 09:59:12 +0100

    fixed offline intent

--- modules/dialogflow_example_intent/src/Plugin/Chatbot/Intent/OfflineIntent.php
+++ modules/dialogflow_example_intent/src/Plugin/Chatbot/Intent/OfflineIntent.php
@@ -8,7 +8,7 @@ use Drupal\chatbot_api\Plugin\IntentPluginBase;
  * Plugin implementation of chatbot intent.
  *
  * @Intent(
- *   id = "projects/restobot-hfb/agent/sessions/d1f46df1-c399-f08b-fcd8-94beda208295/contexts/siteoffline-followup",
+ *   id = "projects/restobot-hfb/agent/intents/31511d13-08fb-4bb9-bd8d-efbb1feffcf5",
  *   label = @Translation("Offline intent")
  * )
  */
